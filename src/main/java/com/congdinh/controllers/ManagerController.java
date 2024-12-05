bae9a6eab76e6a0d5f165577d61ea25830021d1e
Cong Dinh <congdinh2012@hotmail.com>
2024-12-05 18:01:38 +0700

    Step 7: Add Manager Layout and create Footer, Header and Sidebar Components

---
+++ src/main/java/com/congdinh/controllers/ManagerController.java
@@ -0,0 +1,20 @@
+package com.congdinh.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manager")
+public class ManagerController {
+
+    @GetMapping("/category")
+    public String category() {
+        return "manager/category";
+    }
+
+    @GetMapping("/product")
+    public String product() {
+        return "manager/product";
+    }
+}
